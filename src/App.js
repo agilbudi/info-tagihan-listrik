32cb3dff0006796648d525c4ebcbf96ee87ba166
Agil <33302808+agilbudi@users.noreply.github.com>
2022-01-08 17:58:34 +0700

    create project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDfSJSQ1Bs69Jp5_xN4uDcrW05L4jSxB5M",
+  authDomain: "business-project-4040.firebaseapp.com",
+  projectId: "business-project-4040",
+  storageBucket: "business-project-4040.appspot.com",
+  messagingSenderId: "1053247574577",
+  appId: "1:1053247574577:web:0818eca0607ed6c8b2bb10",
+  measurementId: "G-YZHZ2H8R7X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
   return (
